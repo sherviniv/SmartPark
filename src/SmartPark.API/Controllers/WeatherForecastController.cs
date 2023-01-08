b2a267fbc7de4b1010a23cf656386f2d8f7ff071
Shervin <sherviniv@gmail.com>
2023-01-08 22:22:18 +0330

    Add esp32 code

--- src/SmartPark.API/Controllers/WeatherForecastController.cs
+++ src/SmartPark.API/Controllers/WeatherForecastController.cs
@@ -8,38 +8,49 @@ namespace SmartPark.API.Controllers
     public class WeatherForecastController : ControllerBase
     {
         private IHostEnvironment _hostingEnvironment;
+        private readonly ILogger<WeatherForecastController> _logger;
 
-
-        public WeatherForecastController(IHostEnvironment hostingEnvironment)
+        public WeatherForecastController(IHostEnvironment hostingEnvironment, ILogger<WeatherForecastController> logger)
         {
             _hostingEnvironment = hostingEnvironment;
+            _logger = logger;
         }
 
+        private readonly List<string> _AllowedPlateNumbers = new List<string>() { "ABC123", "RAV987" };
         [HttpPost("Upload")]
         public ActionResult UploadImage(IFormFile? imageFile)
         {
-            string uploads = Path.Combine(_hostingEnvironment.ContentRootPath, "uploads");
+            bool allowAccess = false;
+            if (imageFile.Length > 0)
             {
-                if (imageFile.Length > 0)
+                _logger.LogInformation("Image received from device");
+                var textRecognitionService = new TextRecognitionService();
+                using (var ms = new MemoryStream())
                 {
-                    var t = new TextRecognitionService();
-                    using (var ms = new MemoryStream())
+                    imageFile.CopyTo(ms);
+                    var fileBytes = ms.ToArray();
+                    //get plate number from image
+                    var plateNumber = textRecognitionService.RecognizeFromImageAsync(fileBytes).Result;
+                    if (string.IsNullOrEmpty(plateNumber))
                     {
-                        imageFile.CopyTo(ms);
-                        var fileBytes = ms.ToArray();
-                        var a = t.RecognizeFromImageAsync(fileBytes).Result;
-
-
-                        // act on the Base64 data
+                        _logger.LogInformation("No plate-number detected");
                     }
-                    //string filePath = Path.Combine(uploads, Guid.NewGuid()+""+imageFile.FileName);
-                    //using (Stream fileStream = new FileStream(filePath, FileMode.Create))
-                    //{
-                    //    imageFile.CopyToAsync(fileStream);
-                    //}
+                    else
+                    {
+                        _logger.LogInformation($"{plateNumber} detected");
+                        allowAccess = _AllowedPlateNumbers.Contains(plateNumber);
+                    }
                 }
-                return Ok();
+
+                string uploads = Path.Combine(_hostingEnvironment.ContentRootPath, "uploads");
+                string filePath = Path.Combine(uploads, Guid.NewGuid() + "" + imageFile.FileName);
+                using (Stream fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    imageFile.CopyToAsync(fileStream);
+                }
             }
+            return Ok(allowAccess ? "ok" : "no");
+            //return Ok("ok");
         }
     }
 }
--- src/SmartPark.Infrastructure/Services/TextRecognition/TextRecognitionService.cs
+++ src/SmartPark.Infrastructure/Services/TextRecognition/TextRecognitionService.cs
@@ -1,4 +1,7 @@
-﻿using SmartPark.Application.Common.Interfaces;
+﻿using ImageProcessor;
+using ImageProcessor.Imaging.Formats;
+using SmartPark.Application.Common.Interfaces;
+using System.Drawing;
 using System.Reflection;
 using Tesseract;
 
@@ -7,12 +10,12 @@ public class TextRecognitionService : ITextRecognitionService
 {
     private readonly string _testsdataPath;
     public TextRecognitionService()
-	{
+    {
         _testsdataPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, @"Services\TextRecognition\tessdata");
     }
 
-	public Task<string> RecognizeFromImageAsync(byte[] fileBytes) 
-	{
+    public Task<string> RecognizeFromImageAsync(byte[] fileBytes)
+    {
         string text;
         using (var engine = new TesseractEngine(_testsdataPath, "eng", EngineMode.Default))
         {
@@ -26,4 +29,29 @@ public class TextRecognitionService : ITextRecognitionService
         }
         return Task.FromResult(text);
     }
+
+    //public byte[] IncreaseQuality(byte[] photoBytes)
+    //{
+    //    int quality = 99;
+    //    System.Drawing.Imaging.ImageFormat format = System.Drawing.Imaging.ImageFormat.Jpeg;
+    //    Size size = new Size(400, 600);
+    //    using (MemoryStream inStream = new MemoryStream(photoBytes))
+    //    {
+    //        using (MemoryStream outStream = new MemoryStream())
+    //        {
+    //            using (ImageFactory imageFactory = new ImageFactory())
+    //            {
+    //                // Load, resize, set the format and quality and save an image.
+    //                imageFactory.Load(inStream)
+    //                            .Resize(size)
+    //                            .Quality(quality)
+    //                            .GaussianSharpen(1)
+    //                            .Save(outStream);
+    //            }
+
+    //            return outStream.ToArray();
+    //            // Do something with the stream.
+    //        }
+    //    }
+    //}
 }
